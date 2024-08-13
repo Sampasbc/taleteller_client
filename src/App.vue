f600775e44a82bb78b7f610994dd37e1ca2f8e03
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-08-13 17:13:49 +0100

    add logo and icon files

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,15 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <h1>Hello World!</h1>
+  <main class="flex items-center justify-center bg-slate-900 h-[100dvh]">
+    <img
+      src="/public/TaleTeller_Logo.png"
+      alt="TaleTeller Logo"
+      class="size-96"
+    >
+  </main>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<style scoped></style>
