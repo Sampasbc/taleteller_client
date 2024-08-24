dcb4d5daaaa1152558d4a9d191e0a846887adc74
Nando <84581167+Sampasbc@users.noreply.github.com>
2024-08-24 13:19:32 +0100

    feat: create GeneratedContentOptions component

---
+++ src/components/GeneratedContentOptions.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import { Copy, Save, Share2 } from 'lucide-vue-next';
+
+const strokeWidth = 1
+const buttonStyle = 'hover:bg-slate-800 hover:outline hover:outline-4 outline-slate-800 rounded-sm'
+
+</script>
+
+<template>
+  <div class="flex space-x-2 text-slate-200">
+    <button :class="buttonStyle">
+      <Copy :stroke-width="strokeWidth"/>
+    </button>
+    <button :class="buttonStyle">
+      <Save :stroke-width="strokeWidth"/>
+    </button>
+    <button :class="buttonStyle">
+      <Share2 :stroke-width="strokeWidth"/>
+    </button>
+  </div>
+</template>
